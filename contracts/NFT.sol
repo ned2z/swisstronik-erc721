bfba12fe27953e36feaeff0246d7b30fde447b5a
ned2z <139496415+ned2z@users.noreply.github.com>
2024-08-21 11:58:02 +0000

    swisstronik quest

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("SWNed","SWN")
+        ERC721("NedSW","NSW")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
